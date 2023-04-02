3f0411f0599bc53e3c89a8789db4a5b8093590d8
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-03 05:00:37 +0900

    Finished intro

--- JavaScript-Basics/Fundamentals-Part-1/script.js
+++ JavaScript-Basics/Fundamentals-Part-1/script.js
@@ -1,3 +1,20 @@
 let message = "Keep Your Chin Up";
 
 alert(message);
+
+let $ = 1;
+let _ = 2;
+
+alert($ + _);
+
+let admin, name;
+
+name = "John";
+
+admin = name;
+
+alert(admin);
+
+let ourPlanetName = "Earth";
+
+let currentUserName = "Stefan";
