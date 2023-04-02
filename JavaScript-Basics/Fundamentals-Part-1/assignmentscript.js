73b1efb22831427490b3b6f7581e9a24ef834cdf
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-03 06:22:30 +0900

    More examples.

---
+++ JavaScript-Basics/Fundamentals-Part-1/assignmentscript.js
@@ -0,0 +1,15 @@
+console.log(23 + 97);
+
+console.log(8 + 91 + 1991 + 01 + 2021 + 1986);
+
+alert((4 + 6 + 9) / 77);
+
+let a = 10;
+
+console.log(a);
+
+let max = 57;
+
+let actual = max - 13;
+
+let percentage = actual / max;
