6c9d3ece7a5fb2a8db893843d46bd9af7c74f289
Stefan O'Neil <oneil.stefan@gmail.com>
2023-04-06 21:20:18 +0900

    Worked on problems.

---
+++ JavaScript-Basics/foundations-fundamentals-part-3/script.js
@@ -0,0 +1,18 @@
+function add7(number) {
+  return number + 7;
+}
+
+console.log(add7(3));
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(multiply(8, 91));
+
+function capitalize(word) {
+  word.toLowerCase();
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+console.log(capitalize("steFan"));
